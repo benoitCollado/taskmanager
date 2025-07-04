e291969e60786740d9023df6e5091ae2eda57f90
Benoit Collado <benoitcollado@device-202.home>
2025-07-04 09:24:54 +0200

    modification à la marge

--- frontend/taskfront/src/pages/TaskListDetail.vue
+++ frontend/taskfront/src/pages/TaskListDetail.vue
@@ -3,19 +3,16 @@
   import {useTaskStore} from "../store/task"
   import {onMounted,ref,watch, computed} from 'vue';
   import {useRoute} from 'vue-router';
-=======
   import type  {TaskListRead} from '../schemas/TaskList.schema';
   import type {TaskRead} from '../schemas/Task.schema';
   import ZodForm from '../components/ZodForm.vue'
   import {TaskCreateSchema} from '../schemas/Task.schema'
->>>>>>> feature/vuetify
 
   const taskStore = useTaskStore();
   const taskListStore = useTaskListStore();
   const route = useRoute();
   const id = route.params.id;
 
-=======
   const list = computed (()=>{
     return taskListStore.taskLists.find(list => list.id === Number(id));
   })
@@ -26,7 +23,6 @@
   onMounted(async () => {
     await taskStore.fetch(true);
     await taskListStore.fetch(true);
->>>>>>> feature/vuetify
   });
 </script>
 
@@ -36,8 +32,6 @@
     <div v-for="task in tasks" :key="task.id">
       {{task.name}} - status : {{ task.status }} 
     </div>
-=======
     <ZodForm :schema="TaskCreateSchema" :store="{save:taskStore.save, fetch:taskStore.fetch}" :initial-data="{list_id : Number(id), status:'pending'}" @saved="taskStore.fetch(true)" />
->>>>>>> feature/vuetify
   </div>
 </template>
