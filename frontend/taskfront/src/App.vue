b9dc83f5690a84ea9e57e4054a6d758a19a24b55
Benoit Collado <benoitcollado@device-202.home>
2025-07-04 10:26:01 +0200

    suppression des xcaractères indésirables

--- frontend/taskfront/src/App.vue
+++ frontend/taskfront/src/App.vue
@@ -1,14 +1,10 @@
 <template>
-=======
  <nav>
   <button><router-link :to="{name:'TaskLists'}">Acceuil</router-link></button>
  </nav>
   <main>
     <router-view/>
   </main>
-    
-
->>>>>>> feature/vuetify
 </template>
 
 <script setup lang="ts">
